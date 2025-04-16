fb14444f48624cbb0d954a2a436999614ce9f394
João Vitor Marcarini <joaovitormarcarini@gmail.com>
2025-04-16 13:11:31 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "PREÇOS ABSUSIVOS DA NINTENDO SUPREENDE E CAUSA REVOLTA",
+    "description": "A Nintendo pegou a comunidade gamer de surpresa ao anunciar que seus próximos grandes lançamentos para o Nintendo Switch chegarão ao mercado com o preço de $80 dólares. A notícia, quebrando a tradicional faixa de preço de $60 para jogos de console, rapidamente se espalhou e gerou uma intensa reação negativa entre os fãs.\
+    \
+    A justificativa da Nintendo para o aumento não foi imediatamente clara, mas a decisão levanta sérias questões sobre a acessibilidade dos jogos para a popular plataforma híbrida. Muitos jogadores expressaram sua indignação nas redes sociais e fóruns online, argumentando que o novo preço é excessivamente alto, especialmente considerando o poder de hardware do Switch em comparação com consoles de última geração.\
+    \
+    A preocupação não se limita apenas ao valor individual dos jogos, mas também ao potencial impacto no hábito de compra dos consumidores e na saúde geral do mercado de jogos para o Switch. Alguns jogadores ameaçam boicotar os novos lançamentos, enquanto outros questionam se a qualidade dos jogos justificará o aumento significativo no preço.\
+    \
+    A decisão da Nintendo ocorre em um momento delicado, onde o custo de vida e a inflação são preocupações globais. A comunidade gamer aguarda um posicionamento oficial da empresa sobre a mudança de precificação e se haverá alguma estratégia para mitigar o impacto no bolso dos consumidores. A revolta online demonstra claramente que os fãs não receberam bem a notícia, e o futuro dirá como essa decisão afetará as vendas e a imagem da Nintendo.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsDfsjmADTzetx1FKBjOrIjsNGtEG_y5F7og&s"
+  },
+  {
+    "id":"2",
+    "title": "GTA VI anuncia o preço de lançamento, e causa panico!",
+    "description": "A Rockstar Games finalmente revelou o tão aguardado preço de lançamento de Grand Theft Auto VI, e a notícia não foi recebida com a euforia esperada por muitos fãs. Em vez disso, um misto de surpresa e preocupação tomou conta da comunidade gamer. Embora os detalhes exatos do preço variem dependendo da plataforma e edição, os valores anunciados sugerem um patamar significativamente mais alto do que os lançamentos anteriores da franquia, gerando debates acalorados e até mesmo um certo 'pânico' entre os jogadores que já ansiavam por mergulhar no novo mundo do crime da Rockstar. A expectativa em torno do jogo continua altíssima, mas o preço salgado pode influenciar a decisão de compra de muitos, levantando questões sobre a acessibilidade dos jogos AAA na próxima geração.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfpr0v5onLZgbqLe2rbShC6SP1zIpYwl83-g&s"
+  },
+  {
+    "id":"3",
+    "title": "Novo jogo da fromsoftware exclusivo para nintendo switch 2 decepciona!",
+    "description": "A FromSoftware, estúdio renomado por títulos desafiadores e aclamados pela crítica como 'Elden Ring' e 'Sekiro: Shadows Die Twice', anunciou um novo jogo exclusivo para o ainda não lançado Nintendo Switch 2. A expectativa era alta, dada a reputação do estúdio e o potencial do novo hardware da Nintendo. No entanto, o anúncio e as primeiras impressões do título parecem ter causado uma onda de decepção entre os fãs. Seja pela estética, mecânicas de jogo reveladas ou qualquer outro aspecto, o novo exclusivo não parece ter atingido as altas expectativas depositadas nele, levantando questionamentos sobre a estratégia da FromSoftware e da Nintendo para este lançamento tão aguardado. A comunidade agora aguarda por mais detalhes e espera que o jogo possa surpreender positivamente em seu lançamento.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkJrpw4cyp_V7z-mB9mi5IRW5A4dg6qnMf3g&s"
+  },
+  {
+    "id":"4",
+    "title": "Bug onde jogo fica sem som no pc, ainda não resolvido",
+    "description": "O lançamento de 'Assassin's Creed Shadows' para PC tem sido marcado por um problema técnico frustrante para muitos jogadores: a ausência de som em momentos cruciais da jogabilidade. Relatos de usuários inundam fóruns e redes sociais, descrevendo a perda repentina ou intermitente de áudio, impactando a imersão e a experiência geral do jogo. A Ubisoft, desenvolvedora do título, ainda não ofereceu uma solução definitiva para o bug, o que tem gerado crescente impaciência e frustração entre os jogadores de PC que ansiavam por vivenciar a ambientação do Japão feudal. A falta de uma correção rápida levanta preocupações sobre o suporte pós-lançamento e a qualidade da versão para computadores de um dos lançamentos mais aguardados do ano.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn98fYVfF-AmASH62a3yd5vjyUiwbcszyCNA&s"
+  }
+]
